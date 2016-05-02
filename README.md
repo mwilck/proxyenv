7b82b0945a4d68f8a6752bf0cfb3484252cbad6e
Martin Wilck <martin.wilck@ts.fujitsu.com>
2016-05-02 14:55:47 +0200

    Created python setuptools infrastructure

---
+++ README.md
@@ -0,0 +1,44 @@
+# proxyenv: set up a Squid proxy and related process environment
+
+This is a python module that starts a [Squid](http://www.squid-cache.org/) proxy running in a docker container, and sets up an environment for using this proxy to handle HTTP(S) requests both from python code and external programs. The purpose is mainly for testing proxy support in code that needs to make HTTP(S) requests. The proxy supports both HTTP and HTTPS, and Basic proxy authorization.
+
+## Usage
+
+```
+from proxyenv.proxyenv import ProxyFactory
+proxyfact=ProxyFactory()
+
+# Example for using urllib
+from six.moves.urllib import request
+with proxyfact() as proxy:
+    opener  = request.build_opener(proxy.get_handler)
+    opener.open(url)
+
+# Example for starting external program with proxy-related environment
+# variables
+from subprocess import check_output
+with proxyfact(user="johndoe:secret", port=3077) as proxy:
+    check_output(["/usr/bin/wget", url])
+
+```
+Please use the `main` program in `proxyenv/proxyenv.py` as additional source of documentation.
+
+## The proxyenv script
+
+The package also contains a console script `proxyenv`.
+```
+usage: proxyenv [-h] [-p PORT] [-u USER] [-i IMPL] [-s] [-w] [-v]
+                [URL [URL ...]]
+
+positional arguments:
+  URL                   an URL to retrieve
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  proxy port to use (default: 3128)
+  -u USER, --user USER  proxy user in user:password format
+  -i IMPL, --impl IMPL  set implementaton (cmdline or api, default: api)
+  -s, --shell           open a shell in new environment
+  -w, --wait            wait for input
+  -v, --verbose         verbose output
+```
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+import os
+import warnings
+from proxyenv import __version__ as version
+
+def get_description():
+    if (not os.path.exists("README.txt") or
+        (os.path.exists("README.md") and os.stat("README.txt").st_mtime < os.stat("README.md").st_mtime)):
+        try:
+            import pypandoc
+            pypandoc.convert("README.md", "rst", outputfile="README.txt")
+        except (ImportError, OSError, IOError) as exc:
+            warnings.warn("Markdown to RST conversion failed (%s), using plain markdown for description" % exc)
+            return open("README.md", "rt").read()
+    return open("README.txt", "rt").read()
+
+setup(
+    name='proxyenv',
+    version=version,
+    author='Martin Wilck',
+    author_email='mwilck@arcor.de',
+    packages=['proxyenv'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/proxyenv/',
+    license='LICENSE.txt',
+    description='Create HTTP proxy environment using docker',
+    long_description = get_description(),
+    install_requires=[
+        "docker-py >= 1.8",
+        "htpasswd >= 2.3",
+    ],
+    entry_points={
+        'console_scripts': [
+            'proxyenv = proxyenv.proxyenv:main',
+        ]
+    },
+    keywords = 'squid, testing, http, proxy, docker'
+
+    # See https://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Internet :: Proxy Servers'
+    ],
+)
